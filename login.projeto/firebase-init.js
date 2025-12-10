2bbea79a37d1298c54a3b7ceba67646e77afe049
joaomtt123 <joaogabrielmtt86@gmail.com>
2025-12-10 16:51:08 +0000

    s

--- login.projeto/firebase-init.js
+++
@@ -1,9 +0,0 @@
-const firebaseConfig = {
-  apiKey: "AIzaSyBjfSKiB6Ahy0tqDN3TMrw_KCInRcuzjnk",
-  authDomain: "kkkk-d32fe.firebaseapp.com",
-  projectId: "kkkk-d32fe",
-  storageBucket: "kkkk-d32fe.firebasestorage.app",
-  messagingSenderId: "880812625125",
-  appId: "1:880812625125:web:48bd8438c23d5869fc180c",
-  measurementId: "G-JZB9FT1D1F"
-};
